8387c57f451ca40897bd3817f088af56aa3f3ffd
27tj <gamethomes38@gmail.com>
2022-06-23 12:45:21 +0800

    ee

--- src/features/Search_result/Search_result.jsx
+++ src/features/Search_result/Search_result.jsx
@@ -2,6 +2,7 @@ import { useSearchParams } from "react-router-dom";
 import React from "react";
 import Pokemon from "../../components/Pokemon";
 import { gql, useQuery } from "@apollo/client";
+import { Button } from "@mui/material";
 const query = gql`
   query ($pokemon: PokemonEnum!) {
     getPokemon(pokemon: $pokemon) {
@@ -22,10 +23,17 @@ export default function Search_result_Page({ userData, Data }) {
   });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error)
+    return (
+      <div>
+        <div>Pokemon not found</div>
+        <Button variant="contained">Go Back</Button>
+      </div>
+    );
   return (
     <div>
       <Pokemon pokemon={data.getPokemon} />
+      <Button variant="contained"></Button>
     </div>
   );
 }
